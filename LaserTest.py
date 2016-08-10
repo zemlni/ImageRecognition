8182de02a2ccd3f9428833d2c3ece5eebc49ec71
Nikita Zemlevskiy <nikita.zemlevskiy@consumer.org>
2016-08-10 17:04:54 -0400

    add douglasPeucker, function to reduce amount of points on a path. more tests. need to fix douglasPeucker to allow visiting same point twice.

--- LaserTest.py
+++ LaserTest.py
@@ -1,7 +1,8 @@
 #import cv2
 #import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import math
+from collections import OrderedDict
 
 #########################################
 #constants
@@ -9,35 +10,7 @@ SPEED = 10 #pixel/s random speed for now
 WIDTH_OF_CAR = 20 #pixels random width (back wheel to back wheel)
 #########################################
 
-#change this so that it doesn't have to pass through the whole array three times.
-def getLaserPoints(imageArray):
-#        balanceArray = []
-#        for row in imageArray:
-#                for pixel in row:
-#                        average = getAverage(pixel)
-#                        balanceArray.append(average)
-#        balance = 0
-#        for num in balanceArray:
-#                balance += num
-#        balance = balance / len(balanceArray)
-    balance = np.mean(imageArray)
-    light = []
-    dark = []
-    for row in range(0, len(imageArray)):
-            for col in range(0, len(imageArray[row])):
-                pixel = imageArray[row][col]
-                testAverage = getAverage(pixel)
-                if testAverage > balance:
-                    light.append((row, col))
-		    #print str(row) + ", " + str(col) + "light" 
-                else:
-                    dark.append((row, col))
-		    #print "dark"
-    #assumption that laser points take up less space in the picture than non laser.
-    if len(light) > len(dark):
-        return dark
-    else: return light
-
+#find coordinates of laser in a frame
 def getLocation(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     '''
@@ -73,51 +46,68 @@ def getLocation(frame):
 
     return center
 
-def pointsToDirections(laserArray, start, orientation):
+#transform coordinates into directions for each engine
+def pointsToDirections(laserArray, startLocation, startOrientation):
     directions = [] #tuples of format (t1, t2) where t1 = time for engine 1(left) to run, t2 = time for engine 2(right) to run.
-
-    #first maneuver: navigate to first point of path
-    if start != laserArray[0]:
-        #startVector = (start[0], start[1] + 1)#facing forward
-        startVector = orientation
-        #print startVector
-        point = laserArray[0]
-        #print point
-        pointVector = (point[0] - start[0], point[1] - start[1])
-        #print pointVector
-        directions.append(angleTime(startVector, pointVector)) #tells initial angle to turn (if any) 
     
-        startDistanceTime = math.hypot(pointVector[0], pointVector[1]) / SPEED
-        directions.append((startDistanceTime, startDistanceTime)) #investigate whether two motors at once gives different speed - must relate to rpm/SPEED issue
-        orientation = pointVector
-    
-    location = laserArray[0]
-    for i in range(1, len(laserArray)):
-        #prevPoint = laserArray[i - 1]
-        #curPoint = laserArray[i]
-        nextPoint = laserArray[i]
-        #print i
-	if not (location == nextPoint): 
+    location = startLocation
+    orientation = startOrientation
+    for nextPoint in laserArray:
+	if not (location == nextPoint): #checks for zero vectors
             vector1 = orientation 
             vector2 = (nextPoint[0] - location[0], nextPoint[1] - location[1])
+            #get angle to turn, if any
             curAngleTime = angleTime(vector1, vector2)
             if curAngleTime != (0, 0):
                 directions.append(curAngleTime) #tells angle to turn (if any)
-
+            #get distance to move forward, if any
             curDistanceTime = math.hypot(vector2[0], vector2[1]) / SPEED
-            directions.append((curDistanceTime, curDistanceTime)) #investigate whether two motors at once gives different speed - must relate to rpm/SPEED issue
-            #print directions
+            if curDistanceTime != (0, 0):
+                directions.append((curDistanceTime, curDistanceTime)) #investigate whether two motors at once gives different speed - must relate to rpm/SPEED issue
             orientation = vector2
             location = nextPoint #changed location
-            print "location: " + str(location) + " orientation: " + str(orientation)
+            #print "location: " + str(location) + " orientation: " + str(orientation)
             
     return directions        
+
+#simplify curve so that all deviations smaller than epsilon are omitted, should help with jitterings in pixels. 
+#need to find a way to remove consecutive equal points, but not ones that indicate visiting the same spot twice. possibly in perpendicularDistance
+def douglasPeucker(pointList, epsilon):
+    # Find the point with the maximum distance
+    dmax = 0
+    index = 0
+    end = len(pointList) - 1
+    for i in range(1, end):
+        d = perpendicularDistance(pointList[i], (pointList[0], pointList[end])) 
+        if d > dmax: 
+            index = i
+            dmax = d
+    
+    # If max distance is greater than epsilon, recursively simplify
+    if dmax > epsilon:
+        # Recursive call
+        recResults1 = douglasPeucker(pointList[:index + 1], epsilon)
+        recResults2 = douglasPeucker(pointList[index:], epsilon)
+ 
+        # Build the result list
+        resultList = recResults1[:-1] + recResults2 #to avoid having index twice.
+    else:
+        resultList = [pointList[0], pointList[end]]
+    
+    return resultList
+
+#get perpendicular distance between a point and a line
+def perpendicularDistance(point, line): 
+    #a = h*b/2 then h = 2a/b
+    (l1, l2) = line
+    a2 = math.fabs((l2[1] - l1[1]) * point[0] - (l2[0] - l1[0]) * point[1] + l2[0] * l1[1] - l2[1] * l1[0])
+    b = math.hypot(l2[0] - l1[0], l2[1] - l1[1])
+    return a2 / b
 
 #transform angles into times for specific engine to run
 def angleTime(v1, v2): 
     dot = v1[0] * v2[0] + v1[1] * v2[1]
     bottom = math.hypot(v1[0], v1[1]) * math.hypot(v2[0], v2[1])
-    #print "v1: " + str(v1) + "v2: " + str(v2)
     angle = math.acos(dot / bottom)
     time = WIDTH_OF_CAR * angle / SPEED
     answer = (None, None)
@@ -131,31 +121,84 @@ def getAverage(pixel):
     for num in pixel:
             average += num
     return average / len(pixel[:3])
+
 #########################################
 #tests
 #generate points for some shapes
 n = 500 # number of points
-r = 10
+r = 100
 circle = [(math.cos(2 * math.pi / n * x) * r, math.sin(2 * math.pi / n * x) * r) for x in range(0, n + 1)]
 rectangle = []
 for h1 in range(0, 10): rectangle.append((0, h1))
 for l1 in range(0, 20): rectangle.append((l1, 9))
 for h2 in range(9, -1, -1): rectangle.append((19, h2))
 for l2 in range(19, -1, -1): rectangle.append((l2, 0))
+fractions = [(.5, .5), (1.5, 1.5)]
+bigJump = [(0, 0), (100, 0), (0, 0)]
+sameSpot = [(1, 1) for x in range(0, 10)]
 start = (0, 0)
 orientation = (0, 1)
-#print rectangle #rectangle passes.
+
+### tests for pointsToDirections ###
+#print rectangle 
 #print pointsToDirections(rectangle, start, orientation)
-#print circle circle passes
+
+#print circle 
 #print pointsToDirections(circle, start, orientation)
-#sameSpot = [(1, 1) for x in range(0, 10)] #sameSpot passes
+
 #print sameSpot 
 #print pointsToDirections(sameSpot, start, orientation)
-bigJump = [(0, 0), (100, 0), (0, 0)]
-print bigJump
-print pointsToDirections(bigJump, start, orientation)
+
+#print bigJump
+#print pointsToDirections(bigJump, start, orientation)
+
+#print fractions
+#print pointsToDirections(fractions, start, orientation)
+
+### tests for perpendicularDistance ###
+#p1 = (0, 1)
+#line1 = ((-1, 0), (1, 0))
+#print perpendicularDistance(p1, line1)
+
+#p2 = (1, 0)
+#line2 = ((0, 1), (0, -1))
+#print perpendicularDistance(p2, line2)
+#print perpendicularDistance(p2, line1)  
+
+#p3 = (0, 0)
+#line3 = ((0, 1), (1, 0))
+#print perpendicularDistance(p3, line3)
+
+### tests for douglasPeucker ###
+#simpleCircle1 = douglasPeucker(circle, 1)
+#print len(simpleCircle1)
+#print simpleCircle1
+#xs = [x[0] for x in simpleCircle1]
+#ys = [x[1] for x in simpleCircle1]
+#plt.plot(xs, ys, '--')
+#xc = [x[0] for x in circle]
+#yc = [x[1] for x in circle]
+#plt.plot(xc, yc, '-')
+#plt.show()
+
+#print len(douglasPeucker(circle, .5))
+#print len(douglasPeucker(circle, .1))
+#print len(douglasPeucker(circle, 2))
+#print len(douglasPeucker(circle, 5))
+#make more tests to test out reduction of pixel jittering 
+
+#print len(rectangle)
+#rectangle = list(OrderedDict.fromkeys(rectangle)) #introduces bug of never being able to go to the same exact spot twice.
+#print len(douglasPeucker(rectangle, 1))
+#print len(douglasPeucker(rectangle, 2))
+#print len(douglasPeucker(rectangle, .5))
+#print douglasPeucker(rectangle, 1)
+
+#print bigJump
+#print douglasPeucker(bigJump, 1)
 '''
 #########################################
+#main body
 laserArray = []
 cap = cv2.VideoCapture("video2.mp4")
 while not cap.isOpened():
@@ -164,7 +207,7 @@ while not cap.isOpened():
     print "Wait for the header"
 width = cap.get(CV_CAP_PROP_FRAME_WIDTH)
 height = cap.get(CV_CAP_PROP_FRAME_HEIGHT)
-aspectRatio = width/height
+spectRatio = width/height
 
 while True:
     flag, frame = cap.read()
@@ -204,8 +247,9 @@ ys = [x[1] for x in laserArray]
 plt.plot(xs, ys, '-')
 plt.show()
 print laserArray
-
+noDuplicates = list(OrderedDict.fromkeys(laserArray)) #introduces bug of never being able to go to the same exact spot twice.
+simplifiedPath = douglasPeucker(noDuplicates, 1)
 start = (int(width/2), 0) #starting position of the robot, might need to change.
 orientation = (0, 1)
-directions = pointsToDirections(laserArray, start)
+directions = pointsToDirections(simplifiedPath, start)
 '''
