7cf2c7a378183e27e7ed0c735799a2a3a52647a4
Nikita Zemlevskiy <nikita.zemlevskiy@duke.edu>
2017-01-05 00:27:11 -0500

    add PerspectiveTransformCalculator.py. ready for testing

---
+++ PerspectiveTransformCalculator.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+
+im = cv2.imread('test.jpg')
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,127,255,0)
+contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key = cv2.contourArea, reverse = True)
+'''
+for c in contours:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.1 * peri, True)
+    if len(approx) == 4:
+        screenCnt = approx
+        print "TEST"
+        break
+'''
+cnt = contours[1]
+peri = cv2.arcLength(cnt, True)
+approx = cv2.approxPolyDP(cnt, 0.01 * peri, True)
+
+print approx
+#cv2.drawContours(im, [approx], 0, (0,255,0), 3)
+#im = cv2.resize(im, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+#cv2.imshow("TEST", im)
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    #rect = order_points(pts)
+    rect = pts
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
+pts = np.array([(2546, 1323), (2048, 1145), (625, 1177), (179, 1397)], dtype = "float32")
+height, width = im.shape[:2]
+dst = np.array([(2592-108, 1944-57.5), (2592-108, 57.5), (108, 57.5,), (108, 1944-57.5)], dtype="float32")
+M = cv2.getPerspectiveTransform(pts, dst)
+warped = cv2.warpPerspective(im, M, (width, height))
+im = cv2.resize(im, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+
+warped = cv2.resize(warped, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+#cv2.imshow 
+
+print height, width
+cv2.imshow("TEST", warped)
+
+
+cv2.waitKey(0)
